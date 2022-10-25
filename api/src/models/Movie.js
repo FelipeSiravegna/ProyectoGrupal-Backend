3a59ba0f2ba2a8c47b13d03dea7a068daea7e722
Amelfi <amelfi89@gmail.com>
2022-10-25 15:50:16 -0400

    fix misspelled

--- api/src/models/Movie.js
+++ api/src/models/Movie.js
@@ -21,7 +21,7 @@ module.exports = (sequelize) =>{
                 type: DataTypes.STRING,
                 allowNull: false
             },
-            lenguege:{
+            lenguage:{
                 type: DataTypes.STRING,
             },
             realesedDate:{
