fc07516e0ab04f08bd64dcb25cc8fbd0c9121eb1
Amelfi <amelfi89@gmail.com>
2022-10-25 15:34:24 -0400

    fix datatypes integer

---
+++ api/src/models/Movie.js
@@ -0,0 +1,45 @@
+const {DataTypes} = require ('sequelize');
+
+module.exports = (sequelize) =>{
+    sequelize.define('movies',
+        {
+            id:{
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                allowNull: false,
+                primaryKey: true
+            },
+            name:{
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            description:{
+                type:DataTypes.TEXT,
+                allowNull: false
+            },
+            image:{
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            lenguege:{
+                type: DataTypes.STRING,
+            },
+            realesedDate:{
+                type: DataTypes.DATEONLY
+            },
+
+            length:{
+                type: DataTypes.INTEGER
+            },
+            rating:{
+                type: DataTypes.FLOAT
+            },
+            trailer:{
+                type: DataTypes.STRING
+            },
+            saves:{
+                type: DataTypes.INTEGER
+            }
+
+        })
+};
