e77da24cf8874538a6f3ff74d983864a0b18f4ea
J.A.Macri.Ibanez <amacri162013@yahoo.com>
2022-10-26 20:23:08 -0300

    create "controllers/GET/users/index.js"

---
+++ api/src/controllers/GET/users/index.js
@@ -0,0 +1,61 @@
+const { User } = require('../../../db');
+const { Op } = require('sequelize');
+
+
+const getUserById = async(userId)=>{
+    const user = await User.findOne({
+        wher:{
+            id:userId
+        }
+    });
+    return user;
+}
+
+////////Chequear//////////////
+const getUserByNameOrEmail = async (username, email)=>{
+    const user = await User.findOne({
+        where:{
+            [Op.or]:[ { username:username }, {email:email} ]
+        }
+    });
+    return user;
+}
+/////////////////////////////
+
+const getUserByUsername = async (username)=>{
+    const user = await User.findOne({
+        where:{
+            username:username
+        }
+    });
+    return user;
+}
+
+const getUserByEmail = async (email)=>{
+    const user = await User.findOne({
+        where:{
+            email:email
+        }
+    });
+    return user;
+}
+
+const getUsers = async()=>{
+    const users = await User.findAll({
+        where:{
+            banned:null
+        }
+    });
+    return users;
+}
+
+
+
+module.exports={
+    getUserById,
+    getUserByNameOrEmail,
+    getUsers,
+    getUserByUsername,
+    getUserByEmail,
+
+}
