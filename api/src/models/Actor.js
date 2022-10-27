54e960e58583e9ee691462662bfb751b1d12a47d
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-10-27 18:02:07 -0300

    models fix

--- api/src/models/Actor.js
+++ api/src/models/Actor.js
@@ -1,17 +1,16 @@
 const { DataTypes } = require('sequelize');
 
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
-
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('actor', {
     id:{
-        type: DataTypes.UUID,
-         allowNull: false, 
-         primaryKey: true,
-         defaultValue: DataTypes.UUIDV4
-
+      type: DataTypes.UUID,
+        allowNull: false, 
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+    },
+    apiID: {
+      type: DataTypes.INTEGER
     },
     name: {
       type: DataTypes.STRING,
--- api/src/models/List.js
+++ api/src/models/List.js
@@ -1,7 +1,6 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-
     sequelize.define('list', {
         name: { 
             type: DataTypes.STRING,
