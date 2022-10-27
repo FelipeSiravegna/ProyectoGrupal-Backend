469467d77f38df99cdd3f1f2d20a35e9e867ea6d
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-10-26 23:54:26 -0300

    cambios

--- api/src/models/List.js
+++ api/src/models/List.js
@@ -1,39 +1,41 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-
-    sequelize.define('list', {
-        name: { 
-            type: DataTypes.STRING,
-            unique:true,
-            allowNull: false,
-            validate:{
-                len:[0,30]
-            }
+  sequelize.define(
+    "list",
+    {
+      name: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          len: [0, 30],
         },
-        moviesIdList:{
-            type:DataTypes.ARRAY(DataTypes.INTEGER),
-            defaultValue: false,
-            validate:{
-                isInt:true
-            }
+      },
+      moviesIdList: {
+        type: DataTypes.ARRAY(DataTypes.INTEGER),
+        validate: {
+          isInt: true,
         },
-        description:{
-            type: DataTypes.STRING,
-            validate:{
-                len:[0,500]
-            }
+      },
+      description: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [0, 500],
         },
-        ownerUserId:{
-            type:DataTypes.INTEGER,
-            validate:{
-                isInt:true
-            }
+      },
+      ownerUserId: {
+        type: DataTypes.INTEGER,
+        validate: {
+          isInt: true,
         },
-        banned:{
-            type:DataTypes.BOOLEAN,
-        }
-    }, {
-        timestamps: false,
-    });
+      },
+      banned: {
+        type: DataTypes.BOOLEAN,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
 };
