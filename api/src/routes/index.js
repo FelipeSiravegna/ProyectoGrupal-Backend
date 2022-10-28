0afda633d2bc0407fabb0d7d740671fea12c3bf4
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-10-28 15:06:05 -0300

    postMovie route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 const getMoviesPopular = require('./GET/movies/popular.js')
 const searchMovies = require('./GET/movies/search')
 const getMovieDetail = require('./GET/MovieDetail/index');
+const createMovie = require('../routes/POST/Movie');
 
 const router = Router();
 //router.use('/route', archivo route)
 router.use('/movies/popular',getMoviesPopular)
 router.use('/movies/search',searchMovies)
 router.use('/detail', getMovieDetail);
+router.use('/createMovie', createMovie);
 
 module.exports = router;
