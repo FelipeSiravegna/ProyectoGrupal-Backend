d42dec1282bd816dc1047b03e61c6f01cb66e24a
J.A.Macri.Ibanez <amacri162013@yahoo.com>
2022-10-28 23:40:46 -0300

    create api\src\routes\DELETE\user\index.js
    create delete user route (No controllers needed)

---
+++ api/src/routes/DELETE/user/index.js
@@ -0,0 +1,22 @@
+const  { Router } = require("express");
+const { getUserByPk } = require("../../../controllers/GET/users");
+const router = Router();
+
+router.delete("/delete/:userId", async (req, res) => {
+    try {
+        const {userId}=req.params;
+        const user = getUserByPk(userId);
+        if(!user){
+            res.status(400).json({status:400, message:"This user doesn't exist"});
+        }
+        await user.destroy();
+        res.status(200).json({status:200, message:`User ${user.username} deleted`});
+    } catch(error) {
+        console.log(error);
+        console.log("______________________");
+        console.log("                ERROR: "+error.message);
+        res.status(500).json({success:false, message:"There was an error while trying to update the username."});
+    }
+});
+
+module.exports = router
