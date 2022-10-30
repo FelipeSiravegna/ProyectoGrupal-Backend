de20ddc059ec7dd89c92e12c4cd9f16079f36735
J.A.Macri.Ibanez <amacri162013@yahoo.com>
2022-10-30 13:29:28 -0300

    Add getBannedusers middleware

--- api/src/routes/GET/users/index.js
+++ api/src/routes/GET/users/index.js
@@ -1,10 +1,11 @@
 const  { Router } = require("express");
 const getUsers=require("./users");
 const getUser=require("./user");
+const getBannedUsers = require("./bannedUsers");
 const router = Router();
 
 
-router.use("/", getUsers, getUser);
+router.use( "/", getUsers, getUser, getBannedUsers );
 
 
 
