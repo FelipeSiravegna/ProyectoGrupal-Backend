a267f30a20dc57ea0c5cc47a6683b9927073a7cd
Pime-cr <pimentel.crd@gmail.com>
2022-10-31 05:04:45 -0300

    CheckedRoutes

--- api/src/controllers/GET/movies/search.js
+++ api/src/controllers/GET/movies/search.js
@@ -1,12 +1,25 @@
 const axios = require('axios');
-const {Movies,Genre} = require('../../../db.js');
+const {Movies,Genre,Actor,Director} = require('../../../db.js');
 const {API_KEY} = process.env
 const {Op} = require('sequelize')
 
 
-const searchDB = async (name) =>{
-    //const objeto = [name:{ [Op.substring]: name ]
-    const filtredMovies = await Movies.findAll({where:{name:{ [Op.substring]:name }},include:Genre})
+const searchDB = async (name="",actor=[],director=[],genres=[],page=0) =>{
+    console.log('name:',name,'actor:',actor,'director:',director,'genres:',genres,'page:',page)
+    //const objeto=`[Op.and]:[{name:{ [Op.substring]:${name} }},{genres:${genres}}]`
+    //const objeto2 = `{name:{ [Op.substring]:${name} }}'$genres.name$':{ [Op.substring]:genres}}`
+    //console.log(objeto2)
+    const filtredMovies = await Movies.findAndCountAll(
+        {limit:10,
+        offset:page,
+        where:{
+            name:{ [Op.iLike]:`%${name}%`},
+            //'$genres.id$':{ [Op.ne]: 28}            
+        },
+        include:{ 
+            model:Genre ,as : "genres", attributes: ["name","id"] , through: {attributes:[]}},
+        distinct:true
+        })        
     return filtredMovies
 }
 
