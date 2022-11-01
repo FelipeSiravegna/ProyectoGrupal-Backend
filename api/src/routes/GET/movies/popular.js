b8655a15ff36a4485804fbfe931cd350d744f289
Pime-cr <pimentel.crd@gmail.com>
2022-11-01 20:26:57 -0300

    FixedOnRoutes

--- api/src/routes/GET/movies/popular.js
+++ api/src/routes/GET/movies/popular.js
@@ -8,7 +8,7 @@ router.get('/',async (req,res)=>{
     let {page} = req.query
     if (page!=undefined) {page=(page-1)*10}
     try {
-        const DBMovies = await DB_Movies() 
+        const DBMovies = await DB_Movies(page) 
         res.status(200).json(DBMovies)
     } catch (error) {
         res.status(404).send(error.message)
