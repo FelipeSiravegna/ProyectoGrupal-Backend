bf9df12d51146eb6eb9b1059576b50c7ae48b425
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-11-02 19:30:33 -0300

    changes

--- api/src/routes/GET/movies/popular.js
+++ api/src/routes/GET/movies/popular.js
@@ -2,18 +2,27 @@ const { Router } = require("express");
 const router = Router();
 const {DB_Movies} = require('../../../controllers/GET/movies/popular.js')
 
+// router.get('/',async (req,res)=>{
+//     let {page} = req.query
+//     if (page!=undefined) {page=(page-1)*10}
+//     try {
+//         const DBMovies = await DB_Movies(page) 
+//         res.status(200).json(DBMovies)
+//     } catch (error) {
+//         res.status(404).send(error.message)
+//     }
+// })
 
 
 router.get('/',async (req,res)=>{
     let {page} = req.query
     if (page!=undefined) {page=(page-1)*10}
     try {
-        const DBMovies = await DB_Movies() 
+        const DBMovies = await DB_Movies(page) 
         res.status(200).json(DBMovies)
     } catch (error) {
         res.status(404).send(error.message)
     }
 })
-
 
 module.exports= router;
