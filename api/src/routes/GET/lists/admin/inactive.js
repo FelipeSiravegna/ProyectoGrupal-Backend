a08b23e531ffd6d29ce86a15b75c8a6fb5c74b30
J.A.Macri.Ibanez <amacri162013@yahoo.com>
2022-11-04 15:08:52 -0300

    Create inactive.js

---
+++ api/src/routes/GET/lists/admin/inactive.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const router = Router();
+const { getInactiveLists } = require("../../../../controllers/GET/lists");
+
+router.get ("/inactive", async(req, res)=>{
+    try{
+        const bannedLists = await getInactiveLists();
+        if(!bannedLists){
+            res.status(500).json({status:500, message:"There was a problem while trying to get the inactive movie lists"});
+        }
+        else if (!bannedLists.length){
+            res.status(404).json({status:404, message:"There aren't inactive movie lists"});
+        }
+        else{
+            res.status(200).json(lists);
+        }
+    }catch(error){
+        console.log(error)
+        console.log("_____________________________");
+        console.log(error.message);
+        res.status(500).json({status:500, message:"There was a problem while loading the inactive movie lists"});
+    }
+});
+
+module.exports = router;
