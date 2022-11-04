269c4caa83c5383b6e995e7848c3ce7e62acf667
J.A.Macri.Ibanez <amacri162013@yahoo.com>
2022-11-04 15:12:11 -0300

    fix uncalled data

--- api/src/routes/GET/lists/admin/inactive.js
+++ api/src/routes/GET/lists/admin/inactive.js
@@ -5,14 +5,14 @@ const { getInactiveLists } = require("../../../../controllers/GET/lists");
 router.get ("/inactive", async(req, res)=>{
     try{
         const inactiveLists = await getInactiveLists();
-        if(!bannedLists){
+        if(!inactiveLists){
             res.status(500).json({status:500, message:"There was a problem while trying to get the inactive movie lists"});
         }
-        else if (!bannedLists.length){
+        else if (!inactiveLists.length){
             res.status(404).json({status:404, message:"There aren't inactive movie lists"});
         }
         else{
-            res.status(200).json(lists);
+            res.status(200).json(inactiveLists);
         }
     }catch(error){
         console.log(error)
