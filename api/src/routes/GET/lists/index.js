4454967cdeedd73d3cdb3d3c6330b1e84d8b10b2
J.A.Macri.Ibanez <amacri162013@yahoo.com>
2022-11-04 15:06:02 -0300

    Create index.js

---
+++ api/src/routes/GET/lists/index.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const router = Router();
+const public = require("./public");
+const admin = require("./admin");
+
+router.use ("/", admin, public);
+
+module.exports = router;
