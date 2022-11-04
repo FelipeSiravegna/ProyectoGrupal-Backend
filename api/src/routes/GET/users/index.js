3081ca6a47a34c18955884399904260ecf56e63d
J.A.Macri.Ibanez <amacri162013@yahoo.com>
2022-11-03 21:11:52 -0300

    Update index.js

--- api/src/routes/GET/users/index.js
+++ api/src/routes/GET/users/index.js
@@ -1,11 +1,11 @@
 const  { Router } = require("express");
 const getUsers=require("./users");
-const getUser=require("./user");
+const getUserData=require("./user");
 const getBannedUsers = require("./bannedUsers");
 const router = Router();
 
 
-router.use( "/", getUsers, getUser, getBannedUsers );
+router.use( "/", getUsers, getUserData, getBannedUsers);
 
 
 
