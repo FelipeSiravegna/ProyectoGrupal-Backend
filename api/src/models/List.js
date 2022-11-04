16e987a6a73821c638ae53902c970b1a66e7d170
J.A.Macri.Ibanez <amacri162013@yahoo.com>
2022-11-04 15:15:08 -0300

    Update List.js
    set  banned-->defaultValue=false

--- api/src/models/List.js
+++ api/src/models/List.js
@@ -30,6 +30,7 @@ module.exports = (sequelize) => {
         },
         banned:{
             type:DataTypes.BOOLEAN,
+            defaultValue:false
         }
     }, {
         timestamps: false,
