add35d1ee184386231305a1f88d417ec14ca024e
J.A.Macri.Ibanez <amacri162013@yahoo.com>
2022-11-09 14:15:42 -0300

    updated src\controllers\GET\lists\index.js
    Deleted unnecesary functions
    Add folder with correct name (plural sentence "lists" instead of "list")

---
+++ api/src/controllers/GET/lists/index.js
@@ -0,0 +1,78 @@
+const { List, User, Movie } = require("../../../db");
+
+const getListAndContent = async(listId)=>await List.findByPk(listId, {
+    attributes:["id", "name", "description"],
+    through:{
+        attributes:[]
+    },
+    include:{
+        model:Movie,
+        attributes:["id", "name", "image"],
+        through:{
+            attributes:[]
+        }
+    }
+});
+
+const getListByPk = async(listPk)=>await List.findByPk(listPk);
+
+const getUserLists = async(userId)=>{
+    const userLists = await User.findByPk(userId, {
+        attributes:["id", "username", "image", "active", "banned"],
+        through:{
+            attributes:[]
+        },
+        include:{
+            model:List,
+            // where:{
+            //     active:true
+            // },
+            attributes:["id", "name", "description", "active", "banned"],
+            // through:{
+            //     attributes:[]
+            // }
+        }
+    });
+    return userLists;
+};
+
+const getAvailableLists = async()=>await List.findAll({
+     //used to render lists in client side search
+    where:{
+        active:true,
+        banned:false
+    }
+});
+
+const getActiveLists = async()=>await List.findAll({
+    where:{active:true}
+});
+
+const getBannedLists = async()=>await List.findAll({
+    where:{
+        banned:true
+    },
+    exclude:{
+        attributes:["password"]
+    }
+});
+
+const getInactiveLists = async()=>await List.findAll({
+    where:{
+        active:false
+    },
+    exclude:{
+        attributes:["password"]
+    }
+})
+
+module.exports={
+    getListAndContent,
+    getListByPk,
+    getUserLists,
+    getAvailableLists,
+    getActiveLists,
+    getBannedLists, //admin purposes
+    getInactiveLists, //admin purposes
+    
+}
