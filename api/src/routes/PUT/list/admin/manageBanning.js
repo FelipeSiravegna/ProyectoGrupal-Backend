d349310c66b55a48b706ddd2a2e29251eaf29196
J.A.Macri.Ibanez <amacri162013@yahoo.com>
2022-11-09 17:42:08 -0300

    Create manageBanning.js

---
+++ api/src/routes/PUT/list/admin/manageBanning.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const router = Router();
+const { manageListBanning } = require("../../../../controllers/PUT/list");
+
+
+router.put("/manageBanning/:listId", async(req, res)=>{
+    try{
+        const {listId} = req.params;
+        const {action} = req.query;
+        const bannedLists = await manageListBanning(listId, action);
+        if(!bannedLists){
+            res.status(500).json({status:500, message:"There was a problem while trying to get the banned lists"});
+        }
+        else{
+            res.status(bannedLists.status).json(bannedLists);
+        }
+    }catch(error){
+        console.log(error)
+        console.log("_____________________________");
+        console.log(error.message);
+        res.status(500).json({status:500, message:"There was a problem while loading the needed data to handle the banning list"});
+    }
+});
+
+module.exports=router;
