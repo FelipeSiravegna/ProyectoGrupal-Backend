634e37cd80684724073311de796de6d6e54864c0
J.A.Macri.Ibanez <amacri162013@yahoo.com>
2022-11-11 13:09:33 -0300

    Update reviewsController.js

--- api/src/controllers/Reviews/reviewsController.js
+++ api/src/controllers/Reviews/reviewsController.js
@@ -19,11 +19,25 @@ const getComments = async (req, res) => {
 
 const postComments = async (req, res) => {
   try {
-    await Review.create(req.body, {
-      include: [Movie, User],
-    });
-    res.json({mensaje:"done"})
+    const {user, movie}=req.query;
+    //user= the user's id who's doing the review | movie= the movie's id that the user is revewing
+    const { content, userNickName, userImage } = req.body;
+    if(!(user&&movie)){
+      console.log("user and/or movie queries aren't present in the route");
+      res.status(400).json({mensaje:"Oh, no! There's missing info"});
+    }
+    else if(!(userNickName&&userImage)){
+      console.log("userNickName & userImage are mandatory");
+      res.status(400).json({mensaje:"Oh, no! There's missing info"});
+    }
+    else if(!content){
+      res.status(400).json({mensaje:"Is not possible to make an empty comment"});
+    }else{
+      await Review.create({content, userNickName, userImage, userId:user, movieId:movie});
+      res.status(200).json({mensaje:"Review posted successfully"});
+    }
   } catch (error) {
+    console.log(error)
     res.status(404).json({ error: error });
   }
 };
