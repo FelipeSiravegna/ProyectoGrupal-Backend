c2988513a1957e4ebd2ea894e5c52c7251be7715
J.A.Macri.Ibanez <amacri162013@yahoo.com>
2022-11-11 13:18:36 -0300

    add some more validations
    i'll add some validations to the model Review too. The validations of this route are related to the model validations.

--- api/src/controllers/Reviews/reviewsController.js
+++ api/src/controllers/Reviews/reviewsController.js
@@ -30,15 +30,21 @@ const postComments = async (req, res) => {
       console.log("userNickName & userImage are mandatory");
       res.status(400).json({mensaje:"Oh, no! There's missing info"});
     }
+    else if(userImage.length>10000){
+      res.status(400).json({mensaje:"The image route is too long (Max characters allowed: 10 000)"});
+    }
     else if(!content){
       res.status(400).json({mensaje:"Is not possible to make an empty comment"});
+    }
+    else if(content.length>1000){
+      res.status(400).json({mensaje:"The review is too long. (Max characters allowed: 1000)"});
     }else{
       await Review.create({content, userNickName, userImage, userId:user, movieId:movie});
       res.status(200).json({mensaje:"Review posted successfully"});
     }
   } catch (error) {
     console.log(error)
-    res.status(404).json({ error: error });
+    res.status(500).json({ error: "There was a problem while loading the data" });
   }
 };
 
