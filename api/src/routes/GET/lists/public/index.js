8441083b422acc4d1667cb750b331f23f8f0abde
J.A.Macri.Ibanez <amacri162013@yahoo.com>
2022-11-16 13:43:44 -0300

    feature: GET/lists/followedList/:userId

--- api/src/routes/GET/lists/public/index.js
+++ api/src/routes/GET/lists/public/index.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = Router();
-const { getAvailableLists, getListAndContent } = require("../../../../controllers/GET/lists");
+const { getAvailableLists, getListAndContent, getFollwedLists } = require("../../../../controllers/GET/lists");
 
 router.get ("/", async(req, res)=>{
     try{
@@ -36,6 +36,32 @@ router.get("/list/:listId", async(req, res)=>{
         console.log("_____________________________");
         console.log(error.message);
         res.status(500).json({status:500, message:"There was a problem while loading the user movie lists"});
+    }
+});
+
+router.get("/followedLists/:userId", async(req, res)=>{
+    try{
+        const { userId } = req.params;
+        if(userId){
+            const userData = await getFollwedLists(userId);
+            if(!userData){
+                res.status(404).json({status:404, message:"This user doesn't exist"});
+            }
+            else if (!userData.followedLists){
+                res.status(500).json({status:500, message:"There was a problem while loading the followed lists"});
+            }
+            else if(!userData.followedLists.length){
+                res.status(404).json({status:404, message:`The user "${userData.username}" doesn't follow any movie list`});
+            }
+            else{
+                res.status(200).json(userData);
+            }
+        }
+    }catch(error){
+        console.log(error)
+        console.log("_____________________________");
+        console.log(error.message);
+        res.status(500).json({status:500, message:"There was a problem while loading the user followed lists"});
     }
 });
 
